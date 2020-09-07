16889eee08af70a40cc805b8f08baede00a34ab0
atcanbolat <kangalcanbolat@gmail.com>
2020-09-07 16:11:26 -0700

    first commit, setup tracks and users schemas

---
+++ app/tracks/schema.py
@@ -0,0 +1,35 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from .models import Track
+
+class TrackType(DjangoObjectType):
+    class Meta:
+        model = Track
+
+class Query(graphene.ObjectType):
+    tracks = graphene.List(TrackType)
+
+    def resolve_tracks(self, info):
+        return Track.objects.all()
+
+class CreateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        title= graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, title, description, url):
+        user = info.context.user
+
+        if user.is_anonymous:
+            raise Exception('Log in to add a track')
+        
+        track = Track(title=title, description=description, url=url, posted_by=user)
+        track.save()
+        return CreateTrack(track=track)
+
+class Mutation(graphene.ObjectType):
+    create_track = CreateTrack.Field()
