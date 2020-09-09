842cc97c6f2ef63134a65cf75dce56ad0b99cbf4
atcanbolat <kangalcanbolat@gmail.com>
2020-09-08 20:44:23 -0700

    created react front end and linked to python/graphql backend

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,6 +50,8 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +60,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 AUTHENTICATION_BACKENDS = [
---
+++ client/src/Root.js
@@ -0,0 +1,28 @@
+import React from "react";
+import withRoot from "./withRoot";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const GET_TRACKS_QUERY = gql`
+  {
+    tracks {
+      id
+      title
+      description
+      url
+    }
+  }
+`;
+
+const Root = () => (
+  <Query query={GET_TRACKS_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <div>Loading...</div>;
+      if (error) return <div>Error</div>;
+
+      return <div>{JSON.stringify(data)}</div>;
+    }}
+  </Query>
+);
+
+export default withRoot(Root);
