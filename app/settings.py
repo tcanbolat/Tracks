ec86a31c7a7d57d0578c2876c94211cb78d30f95
atcanbolat <kangalcanbolat@gmail.com>
2020-09-17 13:37:23 -0700

    updated middleware order

--- app/settings.py
+++ app/settings.py
@@ -50,10 +50,10 @@ GRAPHENE = {
 }
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
